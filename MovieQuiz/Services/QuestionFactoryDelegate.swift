ca1a45e1426f03e8e01d71dbc89d194d12752700
DGalbacheva <dglbacheva@gmail.com>
2024-02-26 14:40:09 +0300

    Завершенная задача по спринту 5

---
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryDelegate.swift
+//  MovieQuiz
+//
+//  Created by Doroteya Galbacheva on 26.02.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/StatisticService.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticService.swift
+//  MovieQuiz
+//
+//  Created by Doroteya Galbacheva on 26.02.2024.
+//
+
+import Foundation
