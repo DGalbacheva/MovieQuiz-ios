756a5021a0a0fc3c5a602d8cc16545bc92f2c607
DGalbacheva <dglbacheva@gmail.com>
2024-03-19 18:44:43 +0300

    Проведён рефакторинг проекта. Написаны UI и Unit тесты.

---
+++ MovieQuiz/Presentation/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Doroteya Galbacheva on 17.03.2024.
+//
+
+import Foundation
---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  ArrayTests.swift
+//  MovieQuizTests
+//
+//  Created by Doroteya Galbacheva on 18.03.2024.
+//
+
+import Foundation
